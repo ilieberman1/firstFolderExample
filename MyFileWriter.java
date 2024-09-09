0f2ce2d9b8301d20dd9980dfe8b2a4f7dd44c17f
Ian <ilieberman1@hwemail.com>
2024-09-09 00:47:37 -0700

    Uses gitignore now, also create hidden and non-hidden files

--- MyFileWriter.java
+++ MyFileWriter.java
@@ -5,45 +5,31 @@ import java.nio.charset.StandardCharsets;
 
 public class MyFileWriter {
     public static void main(String[] args) {
-        String data = "Hello, World!";
-        String fileName1 = "example1.txt";
-        String fileName2 = "example2.txt";
-        String fileName3 = "example3.txt";
-        String fileName4 = "example4.txt";
-        String fileName5 = "example5.txt";
+        //String data = "Hello, World!";
+        // String fileName1 = "example1.txt";
+        // String fileName2 = "example2.txt";
+        // String fileName3 = "example3.txt";
+        // String fileName4 = "example4.txt";
+        //String fileName5 = "example5.txt";
         //this is my commit today
-
-        // 1. Using FileWriter
-        try (FileWriter writer = new FileWriter(fileName1)) {
-            writer.write(data);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        // 2. Using BufferedWriter
-        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileName2))) {
-            bufferedWriter.write(data);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        // 3. Using FileOutputStream
-        try (FileOutputStream outputStream = new FileOutputStream(fileName3)) {
-            outputStream.write(data.getBytes());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        // 4. Using BufferedOutputStream
-        try (BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(new FileOutputStream(fileName4))) {
-            bufferedOutputStream.write(data.getBytes());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        String password = "myPassword";
 
         // 5. Using Files (java.nio.file)
+        String passwordFile = ".notmypassword.txt";
+        String regularFile = "secretData.dat";
+       
+        String hiddenFolder = ".classifiedData";
+        String topSecretData = "My password is reall just password";
         try {
-            Files.write(Paths.get(fileName5), data.getBytes(StandardCharsets.UTF_8));
+            
+           // Files.write(Paths.get(fileName5), data.getBytes(StandardCharsets.UTF_8));
+            Files.write(Paths.get(passwordFile), password.getBytes(StandardCharsets.UTF_8));
+            Path hiddenFolderName = Files.createDirectory(Paths.get(hiddenFolder));
+            // Files.write(Paths.get(regularFile), topSecretData.getBytes(StandardCharsets.UTF_8));
+            // Files.move(,hiddenFolder.resolve(source.regularFile), REPLACE_EXISTING);
+            Path regularFileName = hiddenFolderName.resolve(regularFile);
+           Files.write(regularFileName, topSecretData.getBytes(StandardCharsets.UTF_8));
+
         } catch (IOException e) {
             e.printStackTrace();
         }
