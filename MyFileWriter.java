e1c6585f606c4527d91182bcbdd97cb61fba4056
Hugh Cheng <hcheng1@hwemail.com>
2024-09-11 10:40:53 -0700

    Fixed totalFileSize
    Moved out of main and added tester

--- MyFileWriter.java
+++ MyFileWriter.java
@@ -5,43 +5,46 @@ import java.nio.charset.StandardCharsets;
 
 public class MyFileWriter {
     public static void main(String[] args) {
-        //String data = "Hello, World!";
+        // String data = "Hello, World!";
         // String fileName1 = "example1.txt";
         // String fileName2 = "example2.txt";
         // String fileName3 = "example3.txt";
         // String fileName4 = "example4.txt";
-        //String fileName5 = "example5.txt";
-        //this is my commit today
+        // String fileName5 = "example5.txt";
+        // this is my commit today
         String password = "myPassword";
 
         // 5. Using Files (java.nio.file)
         String passwordFile = ".notmypassword.txt";
         String regularFile = "secretData.dat";
-       
+
         String hiddenFolder = ".classifiedData";
         String topSecretData = "My password is reall just password";
         try {
-            
-           // Files.write(Paths.get(fileName5), data.getBytes(StandardCharsets.UTF_8));
+
+            // Files.write(Paths.get(fileName5), data.getBytes(StandardCharsets.UTF_8));
             Files.write(Paths.get(passwordFile), password.getBytes(StandardCharsets.UTF_8));
             Path hiddenFolderName = Files.createDirectory(Paths.get(hiddenFolder));
-            // Files.write(Paths.get(regularFile), topSecretData.getBytes(StandardCharsets.UTF_8));
+            // Files.write(Paths.get(regularFile),
+            // topSecretData.getBytes(StandardCharsets.UTF_8));
             // Files.move(,hiddenFolder.resolve(source.regularFile), REPLACE_EXISTING);
             Path regularFileName = hiddenFolderName.resolve(regularFile);
-           Files.write(regularFileName, topSecretData.getBytes(StandardCharsets.UTF_8));
+            Files.write(regularFileName, topSecretData.getBytes(StandardCharsets.UTF_8));
 
         } catch (IOException e) {
             e.printStackTrace();
         }
-        private static void printTotalFileSize(String... fileNames) {
-            long totalSize = 0;
-            for (String fileName : fileNames) {
-                File file = new File(fileName);
-                if (file.exists()) {
-                    totalSize += file.length();
-                }
+        printTotalFileSize("example1.txt", ".classifiedData/secretData.dat");
+    }
+
+    private static void printTotalFileSize(String... fileNames) {
+        long totalSize = 0;
+        for (String fileName : fileNames) {
+            File file = new File(fileName);
+            if (file.exists()) {
+                totalSize += file.length();
             }
-            System.out.println("Total size of all files: " + totalSize + " bytes");
         }
+        System.out.println("Total size of all files: " + totalSize + " bytes");
     }
 }
